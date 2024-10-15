65ffab24b36a00070e96d3f9cf4d20e3cebf8931
sfreytag <simon@freytag.org.uk>
2024-10-15 11:48:09 +0100

    Refactor JsonSchema a little bit to improve readability. Move parser into constructor for reuse.

--- src/Scripts/JsonSchema.php
+++ src/Scripts/JsonSchema.php
@@ -26,6 +26,21 @@ use ReflectionEnum;
 class JsonSchema
 {
     private string $namespace = 'CloudForest\\ApiClientPhp\\Schema\\';
+    private PhpDocParser $phpDocParser;
+    private Lexer $lexer;
+
+    /**
+     * Set up the doc block parser to extract @var tags just once. It can be
+     * used throughout the recursive calls to £this->generate.
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->lexer = new Lexer(false);
+        $constExprParser = new ConstExprParser();
+        $typeParser = new TypeParser($constExprParser);
+        $this->phpDocParser = new PhpDocParser($typeParser, $constExprParser);
+    }
 
     /**
      * Static method to run the generator.
@@ -62,87 +77,111 @@ class JsonSchema
         $reflector = new ReflectionClass($className);
         $properties = $reflector->getProperties();
 
-        // Set up doc block parser to extract @var tags
-        $lexer = new Lexer(false);
-        $constExprParser = new ConstExprParser();
-        $typeParser = new TypeParser($constExprParser);
-        $phpDocParser = new PhpDocParser($typeParser, $constExprParser);
-
-        // Loop through the properties and add them to the schema.
+        // Loop through the properties to add them to the schema. For each
+        // property, get the doc block and extract the @var tags. Ensure there
+        // is only one: it only makes sense to map one var tag to a JSON Schema
+        // property. Then work out what type is in the £var tag and handle it
+        // accordingly.
         foreach ($properties as $property) {
             $propertyName = $property->getName();
 
-            // Get the @var from the doc block for the property
             $docBlock = $property->getDocComment();
             if (!$docBlock) {
                 throw new \Exception('Failed to find doc block for ' . $propertyName);
             }
-            $tokens = new TokenIterator($lexer->tokenize($docBlock));
-            $phpDocNode = $phpDocParser->parse($tokens);
+            $tokens = new TokenIterator($this->lexer->tokenize($docBlock));
+            $phpDocNode = $this->phpDocParser->parse($tokens);
             $vars = $phpDocNode->getVarTagValues();
 
-            // Make sure there's only one. It only makes sense to map one @var to
-            // a JSON Schema property.
             if (count($vars) > 1) {
                 throw new \Exception('Only one @var per property is supported');
             }
             $var = $vars[0];
 
-            // Split up a type using generics and handle it. We only handle
-            // array generics at the moment, ie array<T>
-            //
-            // EG1: An array with an array shape generic, used for coordinates:
-            // array<array{float,float}>
-            // EG2: A list of children:
-            // @var Array<Subcompartment>
-            // EG3: An array of a built-in type
-            // @var Array<string>
             if ($var->type instanceof GenericTypeNode) {
-                $typeName = $var->type->type->name;
-                if ($typeName === 'array') {
-                    $generic = $var->type->genericTypes[0];
-
-                    // Array shape generics
-                    if ($generic instanceof ArrayShapeNode) {
-                        $schema['properties'][$propertyName] = $this->handleType($generic);
-                    }
-
-                    // A list of children
-                    elseif ($generic instanceof IdentifierTypeNode) {
-                        if (class_exists($this->namespace . $generic->name)) {
-                            $schema['properties'][$propertyName] = [
-                                'type' => 'array',
-                                'items' => $this->generate($generic->name),
-                            ];
-                        }
-
-                        // A list of built-ins
-                        else {
-                            $schema['properties'][$propertyName] = [
-                                'type' => 'array',
-                                'items' => $this->handleType($generic),
-                            ];
-                        }
-                    }
-                }
-            }
-
-            // Split up a compound type and handle each one seperately. For example:
-            // @var string|null
-            elseif ($var->type instanceof UnionTypeNode) {
-                $schema['properties'][$propertyName] = ['anyOf' => []];
-                foreach ($var->type->types as $type) {
-                    $schema['properties'][$propertyName]['anyOf'][] = $this->handleType($type);
-                }
-            }
-
-            // Handle everything else. Examples:
-            // @var CompartmentTypeEnum
-            // @var string
-            // @var array{float,float}
-            else {
+                $schema['properties'][$propertyName] = $this->handleGenericTypeNode($var->type);
+            } elseif ($var->type instanceof UnionTypeNode) {
+                $schema['properties'][$propertyName] = $this->handleUnionTypeNode($var->type);
+            } else {
                 $schema['properties'][$propertyName] = $this->handleType($var->type);
             }
+        }
+
+        // Done!
+        return $schema;
+    }
+
+    /**
+     * handleGenericTypeNode
+     * Split up a type using generics and handle it.
+     *
+     * Limitation 1: We only handle array generics at the moment, ie array<T>.
+     *
+     * Limitation 2: The GenericTypeNode has two members, type and genericTypes,
+     * which allows it to support multiple generics like SomethingClever<T1, T2>
+     * Currently we only support a single generic.
+     *
+     * EG1: An array with an array shape generic, used for coordinates:
+     * array<array{float,float}>
+     *
+     * EG2: A list of children from our schema:
+     * [Array<Subcompartment>
+     *
+     * EG3: A list of a built-in type:
+     * Array<string>
+     *
+     * @param GenericTypeNode $type
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    private function handleGenericTypeNode(GenericTypeNode $type)
+    {
+        $schema = [];
+        $typeName = $type->type->name;
+        // Limitation 1: Only support array types with generics
+        if ($typeName === 'array') {
+            // Limitation2: Only support a single generic.
+            $generic = $type->genericTypes[0];
+
+            // EG1: Array shape generics
+            if ($generic instanceof ArrayShapeNode) {
+                $schema = $this->handleType($generic);
+            } elseif ($generic instanceof IdentifierTypeNode) {
+                // EG2: A list of children fro our schema
+                if (class_exists($this->namespace . $generic->name)) {
+                    $schema = [
+                        'type' => 'array',
+                        'items' => $this->generate($generic->name),
+                    ];
+                }
+
+                // EG3: A list of built-ins
+                else {
+                    $schema = [
+                        'type' => 'array',
+                        'items' => $this->handleType($generic),
+                    ];
+                }
+            }
+        }
+        else throw new \Exception('Cannot handle that type yet: ' . $type->type->name);
+        return $schema;
+    }
+
+    /**
+     * handleUnionTypeNode
+     * Split up a compound aka union type and handle each one seperately. For
+     * example:
+     * string|null
+     *
+     * @param UnionTypeNode $type
+     * @return array<mixed>
+     */
+    private function handleUnionTypeNode(UnionTypeNode $type)
+    {
+        $schema = ['anyOf' => []];
+        foreach ($type->types as $type) {
+            $schema['anyOf'][] = $this->handleType($type);
         }
         return $schema;
     }
@@ -214,7 +253,7 @@ class JsonSchema
     }
 
     /**
-     * Cast php types to a JSON schema equivalent
+     * Cast php types to a JSON schema equivalent.
      * @param string $type
      * @return string
      */
